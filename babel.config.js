e6fb036ff03f96c4619f9eb3e6ae4e1b512d0502
Danyil <liberlay555@gmail.com>
2024-04-13 02:13:58 +0300

    Add module-resolver and absolute paths

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            pages: './src/pages',
+            constants: './src/constants',
+            components: './src/components',
+            assets: './public/assets',
+            images: './public/images',
+            fonts: './public/fonts',
+          },
+        },
+      ],
+    ],
+  }
+}
