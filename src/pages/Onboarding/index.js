3e9a908bbbf7b61f4bba9281e49e25354be61c06
Danyil <liberlay555@gmail.com>
2024-04-13 02:14:52 +0300

    Add index file for onboarding pages

---
+++ src/pages/Onboarding/index.js
@@ -0,0 +1,6 @@
+import { FirstPage } from './FirstPage/FirstPage'
+import { SecondPage } from './SecondPage/SecondPage'
+import { ThirdPage } from './ThirdPage/ThirdPage'
+import { FourthPage } from './FourthPage/FourthPage'
+
+export const Onboarding = { FirstPage, SecondPage, ThirdPage, FourthPage }
