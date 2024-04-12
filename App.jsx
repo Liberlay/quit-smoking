7ec3dc179fc9da619670971568e2b6e08c2a4edd
Danyil <liberlay555@gmail.com>
2024-04-13 02:11:29 +0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,45 @@
+import { useFonts } from 'expo-font'
+import { Onboarding } from 'pages/Onboarding'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator()
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    RThin: require('fonts/Roboto-Thin.ttf'),
+    RLight: require('fonts/Roboto-Light.ttf'),
+    RRegular: require('fonts/Roboto-Regular.ttf'),
+    RMedium: require('fonts/Roboto-Medium.ttf'),
+    RBold: require('fonts/Roboto-Bold.ttf'),
+  })
+
+  if (!fontsLoaded) return null
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="FourthPage">
+        <Stack.Screen
+          name="FistPage"
+          component={Onboarding.FirstPage}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SecondPage"
+          component={Onboarding.SecondPage}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ThirdPage"
+          component={Onboarding.ThirdPage}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="FourthPage"
+          component={Onboarding.FourthPage}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
