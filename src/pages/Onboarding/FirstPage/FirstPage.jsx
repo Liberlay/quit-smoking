fa579cb74eb659ee550bb3e6fe3e0f9b0b6bff2a
Danyil <liberlay555@gmail.com>
2024-04-25 16:13:14 +0300

    Change styles

--- src/pages/Onboarding/FirstPage/FirstPage.jsx
+++ src/pages/Onboarding/FirstPage/FirstPage.jsx
@@ -1,5 +1,7 @@
 import { images } from 'constants'
+import { COLORS } from 'constants/theme'
 import { Image, Text, View } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
 import { OnboardingLayout } from 'components/OnboardingLayout/OnboardingLayout'
 
 import styles from './firstPage.style'
@@ -7,12 +9,22 @@ import styles from './firstPage.style'
 export const FirstPage = ({ navigation }) => {
   return (
     <OnboardingLayout title={'Quit now!'} onPress={() => navigation.navigate('SecondPage')}>
-      <Image style={styles.image} source={images.happyMan} />
+      <View style={styles.images_container}>
+        <Image style={styles.image_smoke} source={images.smoke} />
+        <Image style={styles.image_lungs} source={images.lungs} />
+      </View>
       <View style={styles.text_container}>
-        <Text style={styles.text_title}>Congratulations!</Text>
-        <Text style={styles.text_caption}>
-          Quitting smoking is undeniably one of your most significant well-being choices.
-        </Text>
+        <LinearGradient
+          style={styles.wrapper}
+          colors={[COLORS.containerGradientLeft, COLORS.containerGradientRight]}
+          start={{ x: 0, y: 0.5 }}
+          end={{ x: 1, y: 0.5 }}
+        >
+          <Text style={styles.text_title}>Congratulations!</Text>
+          <Text style={styles.text_caption}>
+            Quitting smoking is undeniably one of your most significant well-being choices.
+          </Text>
+        </LinearGradient>
       </View>
     </OnboardingLayout>
   )
--- src/pages/Onboarding/FirstPage/firstPage.style.js
+++ src/pages/Onboarding/FirstPage/firstPage.style.js
@@ -1,25 +1,44 @@
-import { FONT } from 'constants/theme'
 import { StyleSheet } from 'react-native'
+import { COLORS, FONT, SHADOW } from 'constants/theme'
 
 export default styles = StyleSheet.create({
-  image: {
-    width: 351,
-    height: 351,
-    marginTop: 25,
+  images_container: {
+    position: 'absolute',
+    alignItems: 'center',
+  },
+  image_smoke: {
+    position: 'absolute',
+    top: -50,
+    left: -140,
+    width: 675,
+    height: 675,
   },
   text_container: {
+    position: 'absolute',
+    bottom: 160,
+    borderRadius: 20,
+    ...SHADOW,
+  },
+  wrapper: {
     alignItems: 'center',
+    justifyContent: 'center',
     gap: 40,
+    padding: 11,
+    borderRadius: 20,
+    width: 345,
+    height: 210,
   },
   text_title: {
-    fontSize: 35,
+    fontSize: 30,
     fontFamily: FONT.bold,
-    color: 'white',
+    color: COLORS.light,
   },
   text_caption: {
     textAlign: 'center',
     fontSize: 18,
     fontFamily: FONT.regular,
-    color: 'white',
+    lineHeight: 25,
+    letterSpacing: 2,
+    color: COLORS.light,
   },
 })
