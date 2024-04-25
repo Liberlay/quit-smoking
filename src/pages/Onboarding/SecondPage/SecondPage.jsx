5ffe9d73ae4caf7099cd48e2225f6e90dfb4bf19
Danyil <liberlay555@gmail.com>
2024-04-25 16:13:27 +0300

    Change styles

--- src/pages/Onboarding/SecondPage/SecondPage.jsx
+++ src/pages/Onboarding/SecondPage/SecondPage.jsx
@@ -1,26 +1,35 @@
 import { images } from 'constants'
+import { COLORS } from 'constants/theme'
 import { Image, Text, View } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
 import { OnboardingLayout } from 'components/OnboardingLayout/OnboardingLayout'
 
 import styles from './secondPage.style'
 
 export const SecondPage = ({ navigation }) => {
   return (
-    <OnboardingLayout onPress={() => navigation.navigate('ThirdPage')}>
+    <OnboardingLayout
+      onPress={() => navigation.reset({ index: 0, routes: [{ name: 'ThirdPage' }] })}
+    >
       <View style={styles.text_container}>
         <Text style={styles.text_title}>Welcome!</Text>
         <Text style={styles.text_caption}>You're on the right way</Text>
       </View>
-      <Image style={styles.image} source={images.globe} />
-      <View style={styles.bottom}>
-        <View style={styles.bottomText_container}>
+      <Image style={styles.image} source={images.cigarette} />
+      <View style={styles.container}>
+        <LinearGradient
+          style={styles.wrapper}
+          colors={[COLORS.containerGradientLeft, COLORS.containerGradientRight]}
+          start={{ x: 0, y: 0.5 }}
+          end={{ x: 1, y: 0.5 }}
+        >
           <Text style={styles.bottomText}>
             We help people around the world successfully achieve their goals.
           </Text>
           <Text style={styles.bottomText}>
             Let's take the first steps to successfully quit smoking!
           </Text>
-        </View>
+        </LinearGradient>
       </View>
     </OnboardingLayout>
   )
--- src/pages/Onboarding/SecondPage/secondPage.style.js
+++ src/pages/Onboarding/SecondPage/secondPage.style.js
@@ -1,44 +1,50 @@
 import { StyleSheet } from 'react-native'
-import { COLORS, FONT } from 'constants/theme'
+import { COLORS, FONT, SHADOW } from 'constants/theme'
 
 export default styles = StyleSheet.create({
   text_container: {
     alignItems: 'center',
     gap: 10,
-    marginTop: 40,
+    marginTop: 10,
   },
   text_title: {
     fontSize: 35,
     fontFamily: FONT.bold,
-    color: 'white',
+    color: COLORS.dark,
   },
   text_caption: {
     textAlign: 'center',
     fontSize: 28,
     fontFamily: FONT.regular,
-    color: 'white',
+    color: COLORS.dark,
   },
   image: {
-    width: 295,
-    height: 260,
-  },
-  bottom: {
-    alignItems: 'center',
-    gap: 65,
-    width: '100%',
-    height: '100%',
-    paddingHorizontal: 18,
-    backgroundColor: COLORS.background,
-    borderTopLeftRadius: 40,
-    borderTopRightRadius: 40,
-  },
-  bottomText_container: {
-    gap: 20,
-    paddingVertical: 50,
+    transform: [{ rotate: '-45deg' }],
+    width: 350,
+    height: 350,
+    ...SHADOW,
   },
   bottomText: {
-    fontSize: 20,
+    textAlign: 'center',
+    fontSize: 18,
     fontFamily: FONT.regular,
-    color: 'white',
+    lineHeight: 25,
+    letterSpacing: 2,
+    color: COLORS.light,
+  },
+  container: {
+    position: 'absolute',
+    bottom: 160,
+    borderRadius: 20,
+    ...SHADOW,
+  },
+  wrapper: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 40,
+    padding: 11,
+    borderRadius: 20,
+    width: 345,
+    height: 210,
   },
 })
