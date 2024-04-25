ba590b879b0a947a4116d27dc737f185e112ee6a
Danyil <liberlay555@gmail.com>
2024-04-25 16:12:43 +0300

    Add new imports

--- src/pages/Onboarding/index.js
+++ src/pages/Onboarding/index.js
@@ -3,5 +3,15 @@ import { SecondPage } from './SecondPage/SecondPage'
 import { ThirdPage } from './ThirdPage/ThirdPage'
 import { FourthPage } from './FourthPage/FourthPage'
 import { FifthPage } from './FifthPage/FifthPage'
+import { SixthPage } from './SixthPage/SixthPage'
+import { SeventhPage } from './SeventhPage/SeventhPage'
 
-export const Onboarding = { FirstPage, SecondPage, ThirdPage, FourthPage, FifthPage }
+export const Onboarding = {
+  FirstPage,
+  SecondPage,
+  ThirdPage,
+  FourthPage,
+  FifthPage,
+  SixthPage,
+  SeventhPage,
+}
