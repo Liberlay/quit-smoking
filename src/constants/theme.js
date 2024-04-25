badf9bcf80d1201705be71d5f881b037e992dfc5
Danyil <liberlay555@gmail.com>
2024-04-25 16:06:14 +0300

    Add new styles

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,9 +1,12 @@
 const COLORS = {
-  primary: '#0CCE94',
-  border: '#0CCE94',
-  background: '#223935',
-  placeholder: '#D9D9D9',
-  error: 'red',
+  dark: '#273C31',
+  light: '#E5FBE5',
+  error: '#5C1111',
+  errorBorder: '#c91818',
+  bgGradientTop: '#E5FBE5',
+  bgGradientBottom: '#6E8473',
+  containerGradientLeft: '#142120',
+  containerGradientRight: '#273C31',
 }
 
 const FONT = {
@@ -14,4 +17,19 @@ const FONT = {
   bold: 'RBold',
 }
 
-export { COLORS, FONT }
+const SHADOW = {
+  shadowOffset: {
+    width: -8,
+    height: 14,
+  },
+  shadowRadius: 15,
+  shadowOpacity: 0.5,
+  shadowColor: 'black',
+}
+
+const errorBorder = {
+  borderWidth: 3,
+  borderColor: '#c91818',
+}
+
+export { COLORS, FONT, SHADOW, errorBorder }
